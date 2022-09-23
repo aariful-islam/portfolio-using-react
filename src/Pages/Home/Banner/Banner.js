eb0d6cc61dcec65bb67b2e90874cf415e03085cd
arif99050 <arifulislam99050@gmail.com>
2022-09-23 16:26:43 +0600

    full banner done

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -1,60 +1,62 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import { TypeAnimation } from 'react-type-animation';
 import { motion } from "framer-motion";
-// import "../../shared/Shared.css";
+import img from '../../../images/Md_Ariful_Islam-removebg-preview.png'
 
 const Banner = () => {
-  return (
-    <div className="banner min-h-[90vh] flex flex-col-reverse lg:flex-row items-center justify-between">
-      <motion.div
-        initial={{ x: "-100vw" }}
-        animate={{ x: 0 }}
-        transition={{ duration: 1.5 }}
-      >
-        <h2 className="text-neutral text-xl font-medium">Hi, I'm</h2>
-        <h1 className="text-4xl font-semibold mb-0">Ariful Islam</h1>
-        <div className="my-4">
-          <TypeAnimation
-            className="text-2xl text-primary font-bold"
-            cursor={true}
-            sequence={[
-              "A Full stack we b Developer",
-              3000,
-              "A Competitive Programmer",
-              3000,
-              
-            ]}
-            wrapper="div"
-            repeat={Infinity}
-          />
-        </div>
-        <p className="text-neutral max-w-md mb-6 font-medium">
-          I am Jr. Front-end Developer. I am very passionate to my work and
-          dedicated to explore New Tools And Technologies.
-        </p>
+    return (
+        <div className="banner min-h-[90vh] flex flex-col-reverse lg:flex-row items-center justify-between pl-20 pr-20">
+            <motion.div
+                initial={{ x: "-100vw" }}
+                animate={{ x: 0 }}
+                transition={{ duration: 1.5 }}
+            >
+                <h2 className="text-neutral text-xl font-medium">Hi, This is</h2>
+                <h1 className="text-4xl font-semibold mb-0">Ariful Islam</h1>
+                <div className="my-4">
+                    <TypeAnimation
+                        className="text-2xl text-primary font-bold"
+                        cursor={true}
+                        sequence={[
+                            "A Full stack web Developer",
+                            3000,
+                            "A Competitive Programmer",
+                            3000,
 
-        <div className="flex items-center">
-          <a
-            href="https://drive.google.com/file/d/1qVQ1OLO-5RxHy1rwbe5Ib9igCTfeO99f/view?usp=sharing"
-            target="blank"
-          >
-            <button className="primary-button">
-              <span>My Resume</span>
-            </button>
-          </a>
-          <Link to="/about" className="ml-4">
-            <button className="btn border-2 border-primary bg-transparent hover:bg-primary text-white duration-500">
-              About Me
-            </button>
-          </Link>
-        </div>
-      </motion.div>
-      
+                        ]}
+                        wrapper="div"
+                        repeat={Infinity}
+                    />
+                </div>
+                <p className="text-neutral max-w-md mb-6 font-medium">
+                    I'm competitive programmer and full stack web developer. I'm very much pasionate in programming. I want to build amazing websites.I love to explore New Tools And Technologies.
+                </p>
 
-      
-    </div>
-  );
+                <div className="flex items-center">
+                    <a
+                        href="https://drive.google.com/file/d/1CCV7lFfClYR5Fe5Mrrq0GMEIiwjdRHyI/view?usp=sharing"
+                        target="blank"
+                    >
+                        <button className="btn border-2 border-primary bg-transparent hover:bg-primary text-white duration-500">
+                            <span>My Resume</span>
+                        </button>
+                    </a>
+
+                </div>
+            </motion.div>
+            <motion.div
+                className="w-full md:w-1/2"
+                initial={{ x: "100vw" }}
+                animate={{ x: 0 }}
+                transition={{ duration: 1.5 }}
+            >
+                <img src={img} alt="" />
+            </motion.div>
+
+
+
+        </div>
+    );
 };
 
 export default Banner;
